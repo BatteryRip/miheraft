0891766ddf529c1609cbba37d482126153ca4853
BatteryRip <144319777+BatteryRip@users.noreply.github.com>
2024-06-17 17:01:07 +0300

    Add files via upload

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,41 @@
 package com.example.demo;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void buttonClick(ActionEvent event) {
+        Button buttonPage = (Button) event.getSource();
+        String buttonName = buttonPage.getText();
+        if (buttonName.equals("Добавить")) {
+
+        }
+        if (buttonName.equals("Изменить")) {
+
+        }
+        if (buttonName.equals("Отчеты")) {
+
+        }
+        if (buttonName.equals("Категории поиска")) {
+
+        }
     }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
